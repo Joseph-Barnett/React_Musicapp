182a148b897e8abf074c5cd3ec732c065c42c03b
Joseph Barnett <joseph-barnett@outlook.com>
2023-12-05 14:23:12 +0000

    add file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
+import { ArtistDiscovery, ArtistForm, ArtistInfo, ArtistLikeButton } from './components';
 import * as Pages from './pages'
 import './App.css'
 
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { ArtistDiscovery } from './ArtistDiscovery'
+export { ArtistForm } from './ArtistForm'
+export { ArtistInfo } from './ArtistInfo'
+export { ArtistLikeButton } from './ArtistLikeButton'
